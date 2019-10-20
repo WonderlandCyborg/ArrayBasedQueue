bb0be5dc61ce173c9ff98ab5e7c082218796e29e
Allyson Sterling <allysonrs@icloud.com>
2019-10-20 12:16:06 -0700

    Intial upload of array based queue

---
+++ ArrayBasedQueue/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ArrayBasedQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            Console.WriteLine("Hello. Welcome to the Queue. You are in the menu.");
+            while (true)
+            {
+                Console.WriteLine("What would you like to do? Enqueue (e). Dequeue (d). Peek (p). Clear (c). Check if the queue is empty (i).");
+
+                switch (Console.ReadLine())
+                {
+                    case "e":
+                        Console.WriteLine("Great. What Value would you like to enqueue?");
+                        queue.Enqueue(int.Parse(Console.ReadLine()));
+                        break;
+                    case "d":
+                        if (queue.IsEmpty())
+                        {
+                            Console.WriteLine("Great. The Queue is empty. There is nothing left to dequeue.");
+                            break;
+                        }
+                        Console.WriteLine($"Great. {queue.Dequeue()} has beeen dequeued.");
+                        break;
+                    case "p":
+                        if (queue.IsEmpty())
+                        {
+                            Console.WriteLine("Great. The Queue is empty. There is nothing left to peek at.");
+                            break;
+                        }
+                        Console.WriteLine($"Great. {queue.Peek()} is the top value.");
+                        break;
+                    case "c":
+                        Console.WriteLine("Great. The Queue has been cleared.");
+                        queue.Clear();
+                        break;
+                    case "i":
+                        if (queue.IsEmpty())
+                        {
+                            Console.WriteLine("Great. The Queue is empty.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Great. The Queue is not empty.");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ArrayBasedQueue/Queue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayBasedQueue
+{
+    class Queue<T>
+    {
+        public int Count { get; private set; }
+        private T[] data = new T[10];
+        private int head = 0;
+        private int tail = 0;
+        int capacity = 9;
+        int size = 0;
+
+        public Queue()
+            {}
+
+        public void Enqueue (T value)
+        {
+            if(size >= capacity)
+            {
+                Resize(size * 2);
+            }
+            tail %= capacity;//mod size (tail?) to wrap
+            data[tail] = value;
+            tail++;
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            
+            if (size <= capacity/4)
+            {
+                Resize(size / 2);
+            }
+            head %= capacity;//mod size (head?) to wrap
+            T deq = data[head]; 
+            head++;
+            size--;                       
+            return deq; 
+        }
+
+        public T Peek()
+        {
+            return data[head];
+        }
+
+        public void Resize(int Size)// resizing capacity 
+        {
+            T[] temp = new T[Size];
+             //move over, don't move blanks between tail and head
+            data = temp;
+        }
+
+        public bool IsEmpty()
+        {
+            if (size == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
